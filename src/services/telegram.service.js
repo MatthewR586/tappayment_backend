5f6b76563e53387e18e85aa54b003a5fbe4da27c
Matthew Riechers <smartdev1002@gmail.com>
2025-04-22 11:37:15 -0700

    adjust spacing

--- src/services/telegram.service.js
+++ src/services/telegram.service.js
@@ -6,8 +6,7 @@ const sendNotification = (data) => {
     if(data?.body?.type == 'orders.delivery.completed') {
         console.log(data?.body?.data.orderId, (data?.body?.data.payment.received.amount - 2).toFixed(2))
         // send telegram notification 
-        bot.sendMessage(process.env.TELEGRAM_CHAT_ID, `💸 Amount: ${(data?.body?.data.payment.received.amount - 2).toFixed(2)}
-        Order ID: ${data?.body?.data.orderId}`).then(() => console.log("Message sent to channel!")).catch(err => console.log('Error:', err.message));
+        bot.sendMessage(process.env.TELEGRAM_CHAT_ID, `💸 Amount: ${(data?.body?.data.payment.received.amount - 2).toFixed(2)}\nOrder ID: ${data?.body?.data.orderId}`).then(() => console.log("Message sent to channel!")).catch(err => console.log('Error:', err.message));
     }
 };
 
