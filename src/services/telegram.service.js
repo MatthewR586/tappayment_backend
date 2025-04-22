456fd5779249b07391184d37bcf814b910f7d4e8
Matthew Riechers <smartdev1002@gmail.com>
2025-04-22 11:58:18 -0700

    update notificaiton

--- src/services/telegram.service.js
+++ src/services/telegram.service.js
@@ -8,6 +8,7 @@ const sendNotification = (data) => {
         // send telegram notification 
         bot.sendMessage(process.env.TELEGRAM_CHAT_ID, `💸 Amount: ${(data?.body?.data.payment.received.amount - 2).toFixed(2)}\nOrder ID: ${data?.body?.data.orderId}`).then(() => console.log("Message sent to channel!")).catch(err => console.log('Error:', err.message));
     }
+    res.status(200).json({});
 };
 
 module.exports = { sendNotification };
