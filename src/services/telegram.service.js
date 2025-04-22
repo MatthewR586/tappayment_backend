7aa309e18fc2aee9228f74e391367fc3ec0fc1f7
Matthew Riechers <smartdev1002@gmail.com>
2025-04-22 09:43:58 -0700

    update webhook

--- src/services/telegram.service.js
+++ src/services/telegram.service.js
@@ -1,7 +1,13 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: false});
 
 const sendNotification = (data) => {
     if(data?.body?.type == 'orders.payment.succeeded') {
-        console.log(data?.body?.data.orderId, data?.body?.data.payment.received.amount - 2)
+        console.log(data?.body?.data.orderId, (data?.body?.data.payment.received.amount - 2).toFixed(2))
+        // send telegram notification 
+        bot.sendMessage(process.env.TELEGRAM_CHAT_ID, `💸 Amount: ${(data?.body?.data.payment.received.amount - 2).toFixed(2)}
+        Order ID: ${data?.body?.data.orderId}`).then(() => console.log("Message sent to channel!")).catch(err => console.log('Error:', err.message));
     }
 };
 
