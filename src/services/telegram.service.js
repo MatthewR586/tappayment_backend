875fdb2090df1a0b27cf5b3b7962bc0ba7618592
Matthew Riechers <smartdev1002@gmail.com>
2025-04-22 11:12:42 -0700

    udpate message

--- src/services/telegram.service.js
+++ src/services/telegram.service.js
@@ -4,10 +4,10 @@ const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: false});
 
 const sendNotification = (data) => {
     if(data?.body?.type == 'purchase.succeeded') {
-        console.log(data?.body?.data.orderId, (data?.body?.data.payment.received.amount - 2).toFixed(2))
+        console.log(data?.body)
         // send telegram notification 
-        bot.sendMessage(process.env.TELEGRAM_CHAT_ID, `💸 Amount: ${(data?.body?.data.payment.received.amount - 2).toFixed(2)}
-        Order ID: ${data?.body?.data.orderId}`).then(() => console.log("Message sent to channel!")).catch(err => console.log('Error:', err.message));
+        // bot.sendMessage(process.env.TELEGRAM_CHAT_ID, `💸 Amount: ${(data?.body?.data.payment.received.amount - 2).toFixed(2)}
+        // Order ID: ${data?.body?.data.orderId}`).then(() => console.log("Message sent to channel!")).catch(err => console.log('Error:', err.message));
     }
 };
 
