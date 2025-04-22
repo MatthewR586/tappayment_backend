36c137d10c22f75a98138aa57a718021a2c392db
Matthew Riechers <smartdev1002@gmail.com>
2025-04-22 11:19:48 -0700

    update notification

--- src/services/telegram.service.js
+++ src/services/telegram.service.js
@@ -1,13 +1,15 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: false});
-
+let amount = 0;
 const sendNotification = (data) => {
+    if(data?.body?.type == 'orders.payment.succeeded') {
+        amount = data?.body?.data.payment.received.amount - 2;
+    }
     if(data?.body?.type == 'purchase.succeeded') {
         console.log(data?.body)
         // send telegram notification 
-        // bot.sendMessage(process.env.TELEGRAM_CHAT_ID, `💸 Amount: ${(data?.body?.data.payment.received.amount - 2).toFixed(2)}
-        // Order ID: ${data?.body?.data.orderId}`).then(() => console.log("Message sent to channel!")).catch(err => console.log('Error:', err.message));
+        bot.sendMessage(process.env.TELEGRAM_CHAT_ID, `💸 Amount: ${(amount).toFixed(2)}\nOrder ID: ${data?.body?.orderId}`).then(() => console.log("Message sent to channel!")).catch(err => console.log('Error:', err.message));
     }
 };
 
