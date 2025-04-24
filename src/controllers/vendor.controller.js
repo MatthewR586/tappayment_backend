e77be33000a77becc649371b0f48fba38f7794df
Matthew Riechers <smartdev1002@gmail.com>
2025-04-24 07:36:25 -0700

    add db

--- src/controllers/vendor.controller.js
+++ src/controllers/vendor.controller.js
@@ -1,11 +1,16 @@
-const getVendor = (req, res) => {
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const getVendor = async (req, res) => {
     try {
         const { address } = req.query;
         if (!address) throw new Error('Address is required');
-        saveLog(address);
-        res.json({ success: true, address });
+        const venue = await prisma.venue.findUnique({
+            where: {link: address}
+        })
+        res.json({ success: true, message: venue });
     } catch (err) {
-        res.status(400).json({ error: err.message });
+        res.status(400).json({ success: false, message: err.message });
     }
 };
 
