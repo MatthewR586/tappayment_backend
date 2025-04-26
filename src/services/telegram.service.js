ccd98c05c6b558637595693e8fa496a528c70aa0
Matthew Riechers <smartdev1002@gmail.com>
2025-04-26 09:58:27 -0700

    update log

--- src/services/telegram.service.js
+++ src/services/telegram.service.js
@@ -16,7 +16,7 @@ const sendNotification = (data, res) => {
 
 const sendNotificationDev = async (data, res) => {
     try {
-        console.log(data?.body)
+        console.log(data?.body.data.lineItems)
         if(data?.body?.type == 'orders.delivery.completed') {
             const orderWithVenue = await prisma.orderHistory.findUnique({
                 where: {
